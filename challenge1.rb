810f9cb23206e42c12ad08caf4311d794fb415cb
balvarado91@gmail.com <balvarado91@gmail.com>
2014-04-29 16:08:48 -0600

    First Commit In this Repository

---
+++ challenge1.rb
@@ -0,0 +1,164 @@
+		 
+		kind_class = ["1-Warrior", "2-Wizard", "3-Archer"]
+		puts "\n"
+		puts kind_class
+		puts "\n"
+		puts "Choose your role: "
+		puts "\n"
+		option = gets.chomp()
+		puts "\n"
+
+
+		class Hero
+			attr_reader :hp, :attack, :defense  
+			def initialize(hp , attack, defense)
+				@hp = hp
+				@attack = attack
+				@defense = defense			
+			end
+			def attack
+				return @attack
+			end	
+		end	
+
+		class Warrior < Hero
+			def initialize(hp,attack,defense)
+			 super(hp,attack,defense)
+			 
+			end
+		end	 
+
+		class Wizard < Hero
+			def initialize(hp,attack,defense)
+			 super(hp,attack,defense)
+			
+			end
+		end
+
+		class Archer < Hero
+			def initialize(hp,attack,defense)
+			 super(hp,attack,defense)
+		
+			end
+		end
+
+	   class Receive < Hero
+			def initialize(hp,attack,defense)
+				super(hp,attack,defense)		
+				result = attack - (attack * defense/ hp)
+				final = hp - result
+				puts ("#{'Hp'} = #{final}" )
+			end
+		end
+
+		case option
+
+		when "1"
+			#Show the first class Warrior
+			puts "Selected class = [ Warrior ]"
+			puts "\n"
+			
+			oWarrior = Warrior.new(100,15, 40)
+			puts "-------------"
+			puts "|Status|"
+			puts("#{'Hp'} =  #{oWarrior.hp}" )
+			puts("#{'Attack'} =  #{oWarrior.attack}" )
+			puts("#{'Defense'} =  #{oWarrior.defense}" )
+			puts "--------------"
+
+			puts "Do you want to attack? "
+			puts "\n"
+			puts "Press: \n 1 = Yes \n 2 = No"
+			puts "\n"
+			start = gets.chomp()
+
+			case start
+			
+			when "1" 
+				puts "\n"
+				puts ("Ohh you received one attack!!!!")
+				puts "Now your Status is: "
+				oWarrior = Receive.new(oWarrior.hp, oWarrior.attack, oWarrior.defense)
+			when "2"
+				puts "Thank you..You are not ready to fight!!!"
+				
+			end
+			
+			# if kind == 1
+			# 	oWarrior.Receive_Attack
+			# 	puts oWarrior.hp
+			# 	puts  oWarrior.defense
+			# 	elsif kind == 2
+			# 		oArcher.Receive_Attack			
+			# end
+
+		when "2"
+			#Show the second class Wizard
+			puts "Selected class = [ Wizard ]"
+			oWizard = Wizard.new(80,25,68)
+			puts "-------------"
+			puts "|Status|"
+			puts("#{'Hp'} =  #{oWizard.hp}" )
+			puts("#{'Attack'} =  #{oWizard.attack}" )
+			puts("#{'Defense'} =  #{oWizard.defense}" )
+			puts "--------------"
+
+
+			puts "Do you want to attack? "
+			puts "\n"
+			puts "Press: \n 1 = Yes \n 2 = No"
+			puts "\n"
+			start = gets.chomp()
+
+			case start
+			when "1"	
+				puts "\n"
+				puts ("Ohh you received one attack!!!!")
+				puts "Now your Status is: "
+				oWizard = Receive.new(oWizard.hp, oWizard.attack, oWizard.defense)
+			when "2"
+				puts "Thank you..You are not ready to fight!!!"
+			end			
+
+		when "3"
+			#Show the third class Archer
+			puts "Selected class = [ Archer ]"
+			oArcher = Archer.new(92,15,60)
+			puts "-------------"
+			puts "|Status|"
+			puts("#{'Hp'} =  #{oArcher.hp}" )
+			puts("#{'Attack'} =  #{oArcher.attack}" )
+			puts("#{'Defense'} =  #{oArcher.defense}" )
+			puts "--------------"
+			
+
+			puts "Do you want to attack? "
+		    puts "\n"
+			puts "Press: \n 1 = Yes \n 2 = No"
+			puts "\n"		
+			start = gets.chomp()
+
+			case start
+
+			when "1"	
+				puts "\n"
+				puts ("Ohh you received one attack!!!!")
+				puts "Now your Status is: "
+				oArcher = Receive.new(oArcher.hp, oArcher.attack, oArcher.defense)	
+			when "2"
+				puts "Thank you..You are not ready to fight!!!"
+				
+			end
+		end
+
+	
+			
+
+
+
+
+
+		 
+
+
+
